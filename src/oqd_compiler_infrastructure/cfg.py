381cdaad03f63f8d49b8271d6da1ef25cd92735a
yhteoh <yhteoh@uwaterloo.ca>
2026-09-19 11:14:12 -0400

    [fix] Added tags to CFGBlock

--- src/oqd_compiler_infrastructure/cfg.py
+++ src/oqd_compiler_infrastructure/cfg.py
@@ -36,6 +36,7 @@ class CFGBlock(VisitableBaseModel):
     succs: List[int] = Field(default_factory=list)
     exit_nodes: List[int] = Field(default_factory=list)
     edge_labels: Dict[int, str] = Field(default_factory=dict)
+    tags: Dict[str, str] = Field(default_factory=dict)
 
     def add_succ(self, succ: int, label: str | None = None) -> None:
         if succ not in self.succs:
